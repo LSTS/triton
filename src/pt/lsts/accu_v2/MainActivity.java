395629094cb68e2eb40d3de910e81b8132b23a6f
Margarida Faria <margaridaCostaFaria@gmail.com>
2014-01-22 14:24:11 +0000

    Initial commit.

---
+++ src/pt/lsts/accu_v2/MainActivity.java
@@ -0,0 +1,94 @@
+package pt.lsts.accu_v2;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity {
+
+	/**
+	 * Note that this may be null if the Google Play services APK is not
+	 * available.
+	 */
+	private GoogleMap mMap;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// If we're being restored from a previous state,
+		// then we don't need to do anything and should return or else
+		// we could end up with overlapping fragments.
+		if (savedInstanceState != null) {
+			return;
+		}
+
+		setUpMapIfNeeded();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * Sets up the map if it is possible to do so (i.e., the Google Play
+	 * services APK is correctly installed) and the map has not already been
+	 * instantiated.. This will ensure that we only ever call
+	 * {@link #setUpMap()} once when {@link #mMap} is not null.
+	 * <p>
+	 * If it isn't installed {@link SupportMapFragment} (and
+	 * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt
+	 * for the user to install/update the Google Play services APK on their
+	 * device.
+	 * <p>
+	 * A user can return to this FragmentActivity after following the prompt and
+	 * correctly installing/updating/enabling the Google Play services. Since
+	 * the FragmentActivity may not have been completely destroyed during this
+	 * process (it is likely that it would only be stopped or paused),
+	 * {@link #onCreate(Bundle)} may not be called again so we should call this
+	 * method in {@link #onResume()} to guarantee that it will be called.
+	 */
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map_fragment)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				setUpMap();
+			}
+		}
+	}
+
+	/**
+	 * This is where we can add markers or lines, add listeners or move the
+	 * camera. In this case, we just add a marker near Africa.
+	 * <p>
+	 * This should only be called once and when we are sure that {@link #mMap}
+	 * is not null.
+	 */
+	private void setUpMap() {
+		mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title(
+				"Marker"));
+		LatLng sydney = new LatLng(-33.867, 151.206);
+
+		mMap.setMyLocationEnabled(true);
+		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+		mMap.addMarker(new MarkerOptions().title("Sydney")
+				.snippet("The most populous city in Australia.")
+				.position(sydney));
+	}
+
+}
